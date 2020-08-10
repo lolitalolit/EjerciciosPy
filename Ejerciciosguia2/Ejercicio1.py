6dff780e6d24d0b8f9f64345801795668737674e
lolitalolit <torresbarud.elena@gmail.com>
2020-08-10 18:51:32 -0300

    realizo ejercicio 1 de guia 2

--- Ejerciciosguia2/Ejercicio1.py
+++ Ejerciciosguia2/Ejercicio1.py
@@ -2,3 +2,36 @@
 # por pantalla, si tiene entre 50 y 100 caracteres imprimirlo al revés, si no se cumple ninguna de las opciones anteriores, 
 # por pantalla devolver un mensaje que diga "su mensaje es demasiado corto".
 
+
+mensaje = input("Ingrese aquí su mensaje: ")
+
+
+if len(mensaje) >= 100:
+    print(mensaje)
+elif len(mensaje) > 50 and len(mensaje) < 100:
+    print(mensaje[::-1])
+else: 
+    print("Su mensaje es demasiado corto")
+
+# Otra forma
+
+if len(mensaje) > 100:
+    print(mensaje)
+elif 50 <= len(mensaje) < 100:
+    print(mensaje[::-1])
+else: 
+    print("Su mensaje es demasiado corto")
+
+# Otra forma
+
+
+MAX_CHARS = 100
+MIN_CHARS = 50 
+
+
+if len(mensaje) > MAX_CHARS:
+    print(mensaje)
+elif MIN_CHARS <= len(mensaje) < MAX_CHARS:
+    print(mensaje[::-1])
+else: 
+    print("Su mensaje es demasiado corto")
