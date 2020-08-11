d9f27c9a494657458f77fd134eab36f4d41e3ed8
lolitalolit <torresbarud.elena@gmail.com>
2020-08-10 21:24:21 -0300

    hago ejercicio 4 de guia 2

---
+++ Ejerciciosguia2/Ejercicio4.py
@@ -0,0 +1,12 @@
+# Ejercicio 4: Ingresar 6 números por teclado, preferentemente enteros, ordenarlos de manera creciente e imprimirlos por pantalla.
+
+lista = []
+
+
+for i in range(6):
+    numero_ingresado = int(input("Ingrese un número: "))
+    lista.append(numero_ingresado)
+    
+
+lista.sort()
+print(lista)
