783dfd9d9fe5d338fbccb83baf19f8559f15e188
lolitalolit <torresbarud.elena@gmail.com>
2020-08-27 16:03:25 -0300

    creo ejercicio 5

---
+++ Ejerciciosguia2/Ejercicio5.py
@@ -0,0 +1,23 @@
+# Ejercicio 5: Crear un diccionario con los meses del año de la forma { 1: "enero"}. Desafío: lograr cambiar las claves.
+#  Pista: si imprimen ítems del diccionario les crea una lista. Una vez que lo logren, imprimir el diccionario modificado.
+
+meses = {
+    1 : "enero",
+    2 : "febrero",
+    3 : "marzo",
+    4 : "abril",
+    5 : "mayo",
+    6 : "junio",
+    7 : "julio",
+    8 : "agosto",
+    9 : "septiembre",
+    10 : "octubre",
+    11 : "noviembre",
+    12 : "diciembre",
+}
+
+#meses.items()
+
+
+meses.keys()
+print(meses)
